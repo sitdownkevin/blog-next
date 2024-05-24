80cb3ed151ff813dfeb8abe316c3ec4442e90065
XU Ke <kexu567@gmail.com>
2024-05-24 18:21:42 +0800

    Update layout.js

--- src/app/selleverything/layout.js
+++ src/app/selleverything/layout.js
@@ -1,16 +1,21 @@
-
+export const metadata = {
+    title: '毕业卖闲置啦！'
+}
 
 export default function SellEverythingLayout({ children }) {
     return (
         <div>
             <header className="text-center p-16">
-                <h1>从 SCUPI 毕业啦！出闲置啦！</h1>
+                <h1 className="text-gray-500">从 SCUPI 毕业啦！出闲置啦！</h1>
                 <div className="mt-16">
-                    每件物品的详细信息见腾讯文档：<a href="https://docs.qq.com/sheet/DWWJ0dmlpcGxRUkRn">https://docs.qq.com/sheet/DWWJ0dmlpcGxRUkRn</a>
+                    <p>物品的详细信息见</p>
+                    <h3 className="mt-4">
+                        <a className="text-blue-500 underline" href="https://docs.qq.com/sheet/DWWJ0dmlpcGxRUkRn">腾讯文档</a>
+                    </h3>
                 </div>
             </header>
             <div>
-                { children }
+                {children}
             </div>
         </div>
     )
