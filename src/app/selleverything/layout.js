438b367aa8a0527c17a93fe0408d91962210732d
XU Ke <kexu567@gmail.com>
2024-05-25 00:32:19 +0800

    Update layout.js

--- src/app/selleverything/layout.js
+++ src/app/selleverything/layout.js
@@ -8,14 +8,24 @@ export const metadata = {
 
 export default function SellEverythingLayout({ children }) {
     return (
-        <div>
+        <div className="">
             <header className="text-center p-16">
-                <h2 className="text-gray-500 select-none">从 SCUPI 毕业啦！出闲置啦！</h2>
-                <div className="mt-4">
-                    <p className="select-none">物品的详细信息见</p>
-                    <h3 className="mt-4">
-                        <a target="_blank" className="text-blue-500 underline" href="https://docs.qq.com/sheet/DWWJ0dmlpcGxRUkRn">腾讯文档</a>
-                    </h3>
+                <h2 className="font-serif tracking-tight text-gray-500 select-none">
+                    从 SCUPI 毕业啦~
+                </h2>
+                <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <div>
+                        <p className="select-none">物品的图片和详情见</p>
+                        <h3 className="mt-4">
+                            <a target="_blank" className="text-blue-500 underline" href="https://docs.qq.com/sheet/DWWJ0dmlpcGxRUkRn">腾讯文档</a>
+                        </h3>
+                    </div>
+                    <div>
+                        <p className="select-none">物品的添加、删除联系</p>
+                        <h3 className="mt-4">
+                            <span className="select-none">QQ: </span><span className="">1361786946</span>
+                        </h3>
+                    </div>
                 </div>
                 <div className="mt-8 grid grid-cols-2 sm:grid-cols-4 gap-2">
                     <Link href={`/selleverything`} className={buttonVariants({ variant: "outline" })}>全部</Link>
