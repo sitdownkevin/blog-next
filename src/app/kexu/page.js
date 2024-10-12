950a7c6cf625640bf7998d6085b1b64401f46522
Ke Xu <kexu567@gmail.com>
2024-10-12 15:53:59 +0800

    移除获取记录的逻辑及相关内容，添加微信链接部分。

--- src/app/kexu/page.js
+++ src/app/kexu/page.js
@@ -4,32 +4,11 @@ import Link from "next/link"
 
 
 export default async function HiddenPage() {
-    let data = await fetch(
-        "https://api.kexu567.xyz/get_all_records",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            cache: "no-cache",
-        }
-    )
-    let records = await data.json()
-    console.log(records)
+
+
 
     return (
         <div className="flex flex-col space-y-2">
-            <h1>{records.length} Records</h1>
-
-            <div>
-                <h2>Last 5 Records</h2>
-                <ul>
-                    {records.reverse().slice(0, 5).map((record, index) => (
-                        <li key={index}>{(new Date(record.ClickTs)).toLocaleString()} {record.Domain}</li>
-                    ))}
-                </ul>
-            </div>
-
             <Link href={`/kexu/wechat`}>
                 <div className="w-full py-3 flex flex-col items-center justify-center border-2 border-gray-500 hover:bg-gray-400 ">
                     <h1>Wechat</h1>
