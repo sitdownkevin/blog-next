1480ef06b0248625bbb263247010b05e4a84424c
sitdownkevin <kexu567@gmail.com>
2025-01-24 10:06:55 +0800

    Update code

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaGithub, FaLinkedin, FaInstagram, FaWeibo, FaRss } from 'react-icons/fa'
 
+
 const inter = Inter({ subsets: ["latin"] });
 import { Toaster } from '@/components/ui/sonner';
 
@@ -37,11 +38,11 @@ function Header() {
     <div className="logo">
       <h1 className="text-xl font-bold text-gray-500"></h1>
       <div className="flex flex-row items-center space-x-4 w-full">
-          <a href="https://github.com/sitdownkevin" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaGithub /></a>
-          <a href="https://www.linkedin.com/in/sitdownkevin" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaLinkedin /></a>
-          <a href="https://www.instagram.com/sitdownkevin/" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaInstagram /></a>
-          <a href="https://weibo.com/u/5668436889" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaWeibo /></a>
-          <a href="/api/rss" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaRss /></a>
+        <a href="https://github.com/sitdownkevin" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/sitdownkevin" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaLinkedin /></a>
+        <a href="https://www.instagram.com/sitdownkevin/" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaInstagram /></a>
+        <a href="https://weibo.com/u/5668436889" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaWeibo /></a>
+        <a href="/api/rss" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-500 transition-colors duration-300 hover:scale-105"><FaRss /></a>
       </div>
     </div>
     <nav className="flex space-x-4 font-sans text-gray-500">
@@ -63,15 +64,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="min-w-[320px] flex flex-col items-center justify-center w-full h-full">
+        <div className="min-w-[320px] flex flex-col items-center justify-center w-full">
           <div className="w-full md:w-5/6 lg:w-4/6">
             <Header />
           </div>
-          <div className="w-5/6 md:w-4/6 lg:w-3/6 mb-16">
+          <div className="w-5/6 md:w-2/3 lg:w-1/2 mb-16">
             {children}
           </div>
-          <div className="w-5/6 md:w-4/6 lg:w-3/6 mb-8">
-              <Image src={"/favicon-32x32.png"} width={16} height={16} alt="Not Found" />
+          <div className="w-5/6 md:w-2/3 lg:w-1/2 mb-16 flex flex-row justify-center items-center space-x-4">
+            <Image src={"/favicon-32x32.png"} width={16} height={16} alt="" className="rotate-slowly" />
+            <Image src={"/favicon-32x32.png"} width={16} height={16} alt="" className="rotate-slowly" />
+            <Image src={"/favicon-32x32.png"} width={16} height={16} alt="" className="rotate-slowly" />
+            <Image src={"/favicon-32x32.png"} width={16} height={16} alt="" className="rotate-slowly" />
           </div>
         </div>
         <Toaster />
