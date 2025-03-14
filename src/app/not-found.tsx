2f4700d64fe6c9100895df35af710f39f788b2d5
sitdownkevin <kexu567@gmail.com>
2025-03-14 10:34:33 +0800

    Update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,10 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center py-20">
       <Image
-        src={"/meme/st,small,507x507-pad,600x600,f8f8f8.u3.jpg"}
+        src={"/meme/404.png"}
         width={256}
         height={256}
+        className="hover:scale-110 transition-all duration-300 hover:cursor-pointer"
         alt="Not Found"
       />
     </div>
