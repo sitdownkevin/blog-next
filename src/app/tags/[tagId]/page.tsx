b189816c3bef3c91c2ab2af1571883bff6845cac
Ke Xu <kexu567@gmail.com>
2025-04-23 20:47:12 +0800

    Fixed tag error

--- src/app/tags/[tagId]/page.tsx
+++ src/app/tags/[tagId]/page.tsx
@@ -2,13 +2,11 @@ import { getMatterList } from "@/lib/posts/getMatterList";
 import { PostMatterType } from "@/lib/posts/types";
 import { PostCovers } from "@/components/posts/PostCover";
 
-interface TagPageProps {
-  params: {
-    tagId: string;
-  };
+type paramsType = {
+  tagId: string;
 }
 
-export default function TagPage({ params }: TagPageProps) {
+export default function TagPage({ params }) {
   const { tagId } = params;
   const matterList: PostMatterType[] = getMatterList();
 
@@ -22,6 +20,8 @@ export default function TagPage({ params }: TagPageProps) {
     (a, b) => b.update_date.getTime() - a.update_date.getTime()
   );
 
+  console.log("TagPage props:", params);
+  console.log("Filtered matter list length:", sortedFilteredMatterList.length);
   return (
     <div className="w-full flex flex-col">
       <h1 className="text-2xl font-bold mb-4">Posts tagged with "{tagId}"</h1>
