c4426ca5b58d1713d14fd98c4fb9f490fe5e006b
Ke Xu <kexu567@gmail.com>
2025-09-03 21:38:50 +0800

    Update code

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,9 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 
-const baseURL = process.env.NODE_ENV === 'production'
-    ? process.env.BETTER_AUTH_URL
-    : 'http://localhost:3000'
-
 export const authClient = createAuthClient({
-    baseURL
 })
