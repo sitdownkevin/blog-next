18f4ae6a9855561924da6b7b2c1e8fed50dbdcd1
Ke Xu <kexu567@gmail.com>
2025-09-03 21:12:07 +0800

    Update code

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,5 +1,10 @@
 import { createAuthClient } from "better-auth/react"
+
+const baseURL = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `${process.env.BETTER_AUTH_URL}`
+
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL
 })
